c924509ea3ab59b364d43b1935762dfe2fa2c626
Heavenless <54778329+Heavenless@users.noreply.github.com>
2019-11-07 22:55:06 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js");
-const TOKEN = "NjAyODUyMDUyMDM2NzQ3Mjg0.XcPsgw.RIj8ivfbBwjCOb6svdTzTMY9_d8";
+const Discord = require('discord.js')
+const TOKEN = "NjAyODUyMDUyMDM2NzQ3Mjg0.XcRFWQ.gWPD3NQFOykiC09TqE0AuZ_zc0Q";
 const PREFIX = "j!";
 var bot = new Discord.Client();
 const botStats = {};
@@ -7,13 +7,13 @@ const botStats = {};
 
 bot.on("ready", function() {
 
-  let statuses = ['Meliodas V1.0 | Welcomer Bot!', 'welcoming others uwu']
+  let statuses = ['Meliodas V1.0', 'welcoming others uwu']
 
    setInterval(function() {
     
     let status = statuses[Math.floor(Math.random()*statuses.length)]
     
-    bot.user.setPresence({ game: {name: status }, status: 'idle' });
+    bot.user.setPresence({ game: {name: status }, status: 'online' });
   
   }, 5000)
   
@@ -28,14 +28,14 @@ bot.on("ready", function() {
 bot.on('guildMemberAdd', member => {
     let embed = new Discord.RichEmbed()
         .addField("Welcome! To our server 💖 ", member.user.tag)
-        .addField("Has joined, hope u enjoy ur time here!", ":)")
-        .setImage("https://giffiles.alphacoders.com/208/208593.gif")
-        .setFooter("Made by @𝐬𝐩𝐨𝐨𝐤𝐲 𝐡𝐞𝐚𝐯𝐞𝐧𝐥𝐞𝐬𝐬 🎃#5598 with Love! ")
-        .setColor('#FFCCFF');
+        .addField("Has joined, hope you enjoy your time here!", ":)")
+        .setImage("https://media.giphy.com/media/XAZz63Buo8tbjxGRxK/giphy.gif")
+        .setFooter("Made by @𝐡𝐞𝐚𝐯𝐞𝐧𝐥𝐞𝐬𝐬#5598 with Love! ")
+        .setColor('RANDOM');
     let channel = member.guild.channels.find(channel => channel.id === '594428732283027458');
     return channel.send({embed: embed});
 
 });
 
           
-bot.login(TOKEN);
+bot.login(process.env.TOKEN);
